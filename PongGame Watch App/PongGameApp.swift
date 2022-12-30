daa18f7bed483c8e1ec8f2da45758dbfc707cc8d
Andres <frandres333@gmail.com>
2022-12-29 23:48:16 -0500

    Initial Commit

---
+++ PongGame Watch App/PongGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  PongGameApp.swift
+//  PongGame Watch App
+//
+//  Created by Andres Frank on 29/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PongGame_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
